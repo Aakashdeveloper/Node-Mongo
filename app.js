ffbaebaf10067b1aa099cb398381af3754650f89
Aakash Handa <aakashh@aakashh1-linux.gur.corp.google.com>
2016-03-17 16:05:35 +0530

    first

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var http = require('http');
 
 var server = http.createServer(function (req, res){
-	response.writeHead(200, {"Content-type": "text/plain"});
-	response.end("Hello, World");
+	res.writeHead(200, {"Content-type": "text/plain"});
+	res.end("Hello, World");
 	
 	
 });
 
 server.listen(8000);
+console.log("Server running  at http://localhost:8000");
