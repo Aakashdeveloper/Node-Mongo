1c7e42e8555f05294c034a61d4e3f31c8d6c083d
Aakash Handa <aakashh@aakashh1-linux.gur.corp.google.com>
2016-03-22 14:30:56 +0530

    aakash

--- app.js
+++ app.js
@@ -79,3 +79,78 @@ var server = app.listen(3000, function(){
 	
 });
 * */
+
+/* get request*/
+var express = require('express'),
+	app = express(),
+	engines = require('consolidate');
+	
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+
+
+//error handler
+
+function errorHandler(err, req, res, next){
+	console.log(err.message);
+	console.log(err.stack);
+	res.status(500);
+	res.render('error_template', {error:err});
+}
+
+
+
+
+app.use(errorHandler);
+
+app.get('/:name', function(req, res, next){
+	var name = req.params.name;
+	var getvar1 = req.query.getvar1;
+	var getvar2 = req.query.getvar2;
+	res.render('hello', {name :name, getvar1: getvar1, getvar2: getvar2});
+	
+	
+	
+});
+
+app.listen(3000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
