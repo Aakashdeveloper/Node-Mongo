e1e8830e03cf639e6e1a156009fc040dd5ba931a
Aakash Handa <aakashh@aakashh1-linux.gur.corp.google.com>
2016-03-22 14:17:32 +0530

    aakash

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-/* http use
+/*
 var http = require('http');
 
 var server = http.createServer(function (req, res){
@@ -10,8 +10,72 @@ var server = http.createServer(function (req, res){
 
 server.listen(8000);
 console.log("Server running  at http://localhost:8000");
-*/
+
 
 var express = require('express'),
 	con = require('consolidate'),
 	mongodb = require('mongodb');
+*/
+
+
+/* connect to mongodb for shell
+var MongoClient = require('mongodb').MongoClient,
+	assert = require('assert');
+
+MongoClient.connect('mongodb://localhost:27017/video', function(err,db){
+	
+	assert.equal(null, err);
+	
+	db.collection('movies').find({}).toArray(function(err, docs){
+		
+		docs.forEach(function(doc){
+				console.log(doc.title);
+			
+		});
+		
+		
+	});
+	
+	
+	
+});
+*/
+
+/* all together with node html mongodb
+var express = require('express'),
+	app = express(),
+	engines = require('consolidate'),
+	MongoClient = require('mongodb').MongoClient,
+	assert = require('assert');
+	
+app.engine('html', engines.nunjucks);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+MongoClient.connect('mongodb://localhost:27017/video', function(err,db){
+	assert.equal(null, err);
+	
+	app.get('/', function(req, res){
+		db.collection('movies').find({}).toArray(function(err, docs){
+			res.render('hello', {'movies': docs});
+		});	
+	});
+	app.use(function(req, res){
+	    res.sendStatus(404);
+	});
+});
+
+/*app.get('/', function(req, res){
+	res.render('hello', {'name' :'Templates' })
+	
+});
+*/
+
+/* server connect
+var server = app.listen(3000, function(){
+	var port = server.address().port;
+	console.log('Server running  at http://localhost %s', port);
+	
+	
+});
+* */
