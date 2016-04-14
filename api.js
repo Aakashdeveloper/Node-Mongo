83f3123e420d288262eadbe40c1eae764c3e6e6c
Aakash Handa <aakashh@aakashh1-linux.gur.corp.google.com>
2016-04-14 15:23:28 +0530

    aakk

--- api.js
+++ api.js
@@ -1,5 +1,4 @@
 var express = require("express"),
-<<<<<<< HEAD
 	mongoose = require("mongoose");
 	
 var db = mongoose.connect('mongodb://localhost/bookApi');
@@ -13,9 +12,15 @@ var bookRouter = express.Router();
 
 bookRouter.route('/Books')
 	.get(function(req, res){
-		Book.find(function(err,books){
+		
+		var query ={};
+		if(req.query.genre)
+		{
+			query.genre = req.query.genre;
+		}
+		Book.find(query, function(err,books){
 			if(err)
-				console.log(err);
+				res.status(500).send(err);
 			else
 				res.json(books);	
 		});
@@ -31,21 +36,4 @@ app.get('/', function(req, res){
 app.listen(port, function(){
 	console.log("server running on port" +port);
 });
-=======
-    gulp = require("gulp"),
-    nodemon = require('gulp-nodemon");
-    
-gulp.task['default', function(){
-	        nodemon({
-			script: "app.js",
-			ext: "js",
-			env:{
-				PORT:8000
-			},
-			ignore: ['/node_modules/..']
-		});
-		.on('restart', function(){
-				console.log("restart");
-		});
-}];
->>>>>>> 66acb6304f36a586b5399d1396fe539a86317093
+
