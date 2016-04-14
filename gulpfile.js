83dd0bbd8fcd1ba030d7727122cd07d9485ca6be
Aakash Handa <aakashh@aakashh1-linux.gur.corp.google.com>
2016-04-14 14:22:32 +0530

    aakash

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var    gulp = require("gulp"),
+    nodemon = require('gulp-nodemon");
+    
+gulp.task['default', function(){
+	        nodemon({
+			script: "api.js",
+			ext: "js",
+			env:{
+				PORT:8000
+			},
+			ignore: ['/node_modules/..']
+		});
+		.on('restart', function(){
+				console.log("restart");
+		});
+}];
--- simpleapi.js
+++ simpleapi.js
@@ -1,23 +1,15 @@
 var express = require("express"),
-	mongoose = require("mongoose");
-	
-var db = mongoose.connect('mongodb://localhost/bookApi');
-
-var Book = require('./model/bookModel');
 
 var app = express();
 var port = 3000;
 
 var bookRouter = express.Router();
 
-bookRouter.route('/Books')
+bookRouter.route('/Book')
 	.get(function(req, res){
-		Book.find(function(err,books){
-			if(err)
-				console.log(err);
-			else
-				res.json(books);	
-		});
+		var responseJson = (hello: "this is my api");
+		
+		res.json(responseJson);
 	});
 
 app.use('/app', bookRouter);
